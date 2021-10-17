17c2f326597787f985d5b535d5b0de5d0c3a219b
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-17 19:19:57 +0530

    server.js refactored

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
+const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json())
 
