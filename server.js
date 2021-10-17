1e3a54a73a46ed141af598849b8e7aac69e0e693
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-17 19:19:40 +0530

    server.js refactored

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const mongooseFieldEncryption =
-  require("mongoose-field-encryption").fieldEncryption;
-var session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json())
@@ -14,18 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const correctionRoutes = require("./routes/corrections");
 const postsRoutes = require("./routes/posts");
 const signUpRoutes = require("./routes/signUp");
-
-app.use(
-  session({
-    secret: "Hello World",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-app.use(passport.initialize());
-
-app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/visionDB", {
   useNewUrlParser: true,
@@ -35,7 +18,5 @@ mongoose.connect("mongodb://localhost:27017/visionDB", {
 app.use(correctionRoutes);
 app.use(postsRoutes);
 app.use(signUpRoutes);
-
-// app.use(errorController.error);
 
 app.listen(3000, () => console.log("The server is running on port 3000"));
