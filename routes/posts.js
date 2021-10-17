c3d4e81fd8b2db95d5842194ddf91540ea58a9f6
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-17 19:06:53 +0530

    posts route refactored

--- routes/posts.js
+++ routes/posts.js
@@ -1,27 +1,8 @@
-const path = require("path");
-
 const express = require("express");
-
-const rootDir = require("../util/path");
 
 const router = express.Router();
 
-const labelSchema = require("../models/label");
-
-const mongooseFieldEncryption =
-  require("mongoose-field-encryption").fieldEncryption;
-var md5 = require("md5");
-var bcrypt = require("bcryptjs");
-console.log(md5("message"));
-var session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-
 const postsController = require("../controllers/postsController");
-const mongoose = require("mongoose");
-// const {Schema} = mongoose;
-
-// const Label = mongoose.model("Label", labelSchema);
 
 // Post Request to store label Details(predictions by the ML model)
 
