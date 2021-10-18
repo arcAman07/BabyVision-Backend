ee6936965b8468029560368fc1fb8b3b2c978f87
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-18 14:51:49 +0530

    user model refactored

--- models/user.js
+++ models/user.js
@@ -1,22 +1,17 @@
-const fs = require('fs');
-const path = require('path');
+const path = require("path");
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
+  "data",
+  "products.json"
 );
 
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
-const userSchema = new Schema(
-    {
-        name:{ type:'string', required: true},
-        email:{ type:'string', required: true},
-        username: {type: 'string',
-            required: true},
-        password: {type: 'string',required: true},
-}
-
-);
+const { Schema } = mongoose;
+const userSchema = new Schema({
+  name: { type: "string", required: true },
+  email: { type: "string", required: true },
+  username: { type: "string", required: true },
+  password: { type: "string", required: true },
+});
 
 module.exports = userSchema;
