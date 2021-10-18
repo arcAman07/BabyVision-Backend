1926e26ab556a404e3697859765375e24d9060e1
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-18 14:52:47 +0530

    label model refactored

--- models/label.js
+++ models/label.js
@@ -1,25 +1,18 @@
-const fs = require('fs');
-const path = require('path');
+const path = require("path");
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
+  "data",
+  "products.json"
 );
 
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
-const labelSchema = new Schema(
-    {
-        name:{ type:'string', required: true},
-        username: {type: 'string',
-            required: true},
-        picture:{ type:'string'},
-        label:{type:'string'},
-
-
-}
-
-);
+const { Schema } = mongoose;
+const labelSchema = new Schema({
+  name: { type: "string", required: true },
+  username: { type: "string", required: true },
+  picture: { type: "string" },
+  label: { type: "string" },
+});
 
 module.exports = labelSchema;
