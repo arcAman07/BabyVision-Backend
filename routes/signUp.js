2f7300e3ad68153885d926bab5ce1264dc86fd4e
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-18 13:59:48 +0530

    signUp route  refactored

--- routes/signUp.js
+++ routes/signUp.js
@@ -6,19 +6,8 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
-// const userSchema = require('../models/user');
-
 const mongoose = require("mongoose");
-// const {Schema} = mongoose;
 
-// userSchema.plugin(mongooseFieldEncryption,{
-//     fields:["password"],
-//     secret:process.env.SECRET,
-//     saltGenerator: (secret) => secret.slice(0, 16)
-
-// })
-
-// const User = mongoose.model("User", userSchema);
 const signUpcontroller = require("../controllers/signUpcontroller");
 
 // Post Request to store user Details
