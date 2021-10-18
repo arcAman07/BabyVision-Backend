ed5ffcc6a29613f586c33f6812d9a16b539f1465
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-18 14:50:30 +0530

    corrections route  refactored

--- routes/corrections.js
+++ routes/corrections.js
@@ -6,21 +6,6 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
-// const correctionSchema = require('../models/labelCorrection');
-
-const mongooseFieldEncryption =
-  require("mongoose-field-encryption").fieldEncryption;
-var md5 = require("md5");
-var bcrypt = require("bcryptjs");
-console.log(md5("message"));
-var session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-const mongoose = require("mongoose");
-// const {Schema} = mongoose;
-
-// const Correction = mongoose.model("Correction", correctionSchema);
-
 const correctionsController = require("../controllers/correctionsController");
 
 // Post Request to store the accuracy of the label Details(predicted by the ML model)
