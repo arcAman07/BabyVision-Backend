ff8eb16e514a398cfc2c197befbc17526dfd7bfd
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-18 14:52:20 +0530

    label correction model refactored

--- models/labelCorrection.js
+++ models/labelCorrection.js
@@ -1,27 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+const path = require("path");
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
+  "data",
+  "products.json"
 );
 
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
-const correctionSchema = new Schema(
-    {
-        name:{ type:'string', required: true},
-        username: {type: 'string',
-            required: true},
-        picture:{ type:'string'},
-        expectedLabel:{type:'string'},
-        label:{type:'string'},
-        accuracy:{type:'boolean'},
-        feedback:{type:'string'}    
-
-}
-
-);
+const { Schema } = mongoose;
+const correctionSchema = new Schema({
+  name: { type: "string", required: true },
+  username: { type: "string", required: true },
+  picture: { type: "string" },
+  expectedLabel: { type: "string" },
+  label: { type: "string" },
+  accuracy: { type: "boolean" },
+  feedback: { type: "string" },
+});
 
 module.exports = correctionSchema;
